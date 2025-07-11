73f9170bab3d1befeb13f256e7aa03145dfb77a9
Calvin James Maximo <calvinjames.maximo@gcash.com>
2025-07-11 16:22:21 +0800

    feat: add constructor, getter, and new attribute to Enclosures

--- src/main/java/org/example/buildings/Enclosures.java
+++ src/main/java/org/example/buildings/Enclosures.java
@@ -2,4 +2,18 @@ package org.example.buildings;
 
 public class Enclosures extends Buildings {
     protected String species;
+    private String animal;
+
+    public Enclosures(String species, String animal) {
+        this.species = species;
+        this.animal = animal;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public String getAnimal() {
+        return animal;
+    }
 }
