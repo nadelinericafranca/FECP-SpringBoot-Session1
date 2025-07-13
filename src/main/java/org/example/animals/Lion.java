aa88bb00d2273458ef4b5f523db5d2b5422e8949
Nadeline Ricafranca <nadeline.ricafranca@gcash.com>
2025-07-13 09:43:18 +0800

    feat: add Lion class

---
+++ src/main/java/org/example/animals/Lion.java
@@ -0,0 +1,22 @@
+package org.example.animals;
+
+public class Lion extends Feline {
+    public Lion(String name, String species) {
+        super(name, species);
+    }
+
+    @Override
+    public void eat() {
+
+    }
+
+    @Override
+    public void sleep() {
+
+    }
+
+    @Override
+    public void makeSound() {
+
+    }
+}
