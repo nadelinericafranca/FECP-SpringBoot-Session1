a84412280c12d99f139d55af3554412b0fd4530b
Nadeline Ricafranca <nadeline.ricafranca@gcash.com>
2025-07-13 09:30:31 +0800

    feat: implement methods heal() and lecture()

--- src/main/java/org/example/people/Veterinarians.java
+++ src/main/java/org/example/people/Veterinarians.java
@@ -1,6 +1,9 @@
 package org.example.people;
 
+import org.example.animals.Animal;
 import org.example.buildings.Buildings;
+
+import java.time.LocalDateTime;
 
 public class Veterinarians extends People {
     public Veterinarians(String name) {
@@ -16,11 +19,15 @@ public class Veterinarians extends People {
 
     }
 
-    public static void heal() {
-
+    public void heal(Animal animal) {
+        System.out.println("Dr. " + name + " begins healing sick animals...");
+        animal.setHealthy(true);
+        animal.setHealingTimestamp(LocalDateTime.now()); // Set timestamp
+        System.out.println("✅ Healed: " + animal.getName());
+        System.out.println(animal.getName() + " has been discharged and returned to enclosure.");
     }
 
-    public static void lecture() {
-
+    public void lecture() {
+        System.out.println("Dr. " + name + "gives a science lecture on animal health and conservation.");
     }
 }
